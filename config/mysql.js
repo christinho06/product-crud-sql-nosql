4014bd77ed19b38734a4ae49c752d98f288d5656
christinho06 <duchelondziel@gmail.com>
2026-06-16 11:09:58 +0000

    Add MySQL pool config

---
+++ config/mysql.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '',
+  database: process.env.MYSQL_DB || 'productdb'
+});
+module.exports = pool;
